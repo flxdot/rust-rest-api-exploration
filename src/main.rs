b221c6a2d717f0e4f0d3738dda455a9093055e61
Felix Fanghaenel <felix.fanghaenel@nitrex.com>
2023-04-11 08:43:47 +0200

    add OAuth2 flow to SwaggerUI

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,11 @@ mod services;
 
 #[tokio::main]
 async fn main() {
-    std::env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "tower_http=debug");
+    };
 
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .init();
+    tracing_subscriber::fmt().init();
 
     let mut api = OpenApi::default();
     let app = services::build_router()
@@ -51,11 +51,19 @@ fn api_docs(api: TransformOpenApi) -> TransformOpenApi {
         })
         .security_scheme(
             "ApiKey",
-            aide::openapi::SecurityScheme::ApiKey {
-                location: aide::openapi::ApiKeyLocation::Header,
-                name: "X-Auth-Key".into(),
-                description: Some("A key that is ignored.".into()),
-                extensions: Default::default(),
+            aide::openapi::SecurityScheme::OAuth2 {
+                flows: aide::openapi::OAuth2Flows {
+                    implicit: None,
+                    password: Some(aide::openapi::OAuth2Flow::Password {
+                        refresh_url: Some(String::from("https://sso.test.com/refresh")),
+                        token_url: String::from("https://sso.test.com/login"),
+                        scopes: indexmap::IndexMap::new(),
+                    }),
+                    authorization_code: None,
+                    client_credentials: None
+                },
+                description: Some(String::from("Defines the way you may authorize against the API.")),
+                extensions: indexmap::IndexMap::new(),
             },
         )
         .default_response_with::<Json<AppError>, _>(|res| {
